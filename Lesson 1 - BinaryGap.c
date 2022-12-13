a74d77f8c5ac9a998771d0d9b09fdd6a3ae72752
AliBh14 <120137525+AliBh14@users.noreply.github.com>
2022-12-13 19:33:10 +0330

    Add files via upload

--- Lesson 1 - BinaryGap.c
+++ Lesson 1 - BinaryGap.c
@@ -2,8 +2,7 @@
 Find longest sequence of zeros in binary representation of an integer. */
 
 int solution(int N) {
-    // write your code in C99 (gcc 6.2.0)
-    signed int i, j, max_length = 0, tmp_max_length = 0;
+    signed int i, max_length = 0;
     signed int tmp_pos1 = -1, pos1 = 32;
     for(i = 0; i < 32; i++){
         if(((N >> i) & 1) == 1){
